b0eb860c2c2455f9a64adb0d8ec850ba879873b0
guruusingh2033 <guruu.singh2033@gmail.com>
2018-08-06 20:14:45 +0530

    new changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -62,58 +62,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   selectedJobStatus: any;
   templateMsg: String;
 
-  // statesComplex: any[] = [
-  //   { id: 1, name: 'Alabama', region: 'South' },
-  //   { id: 2, name: 'Alaska', region: 'West' },
-  //   { id: 3, name: 'Arizona', region: 'West' },
-  //   { id: 4, name: 'Arkansas', region: 'South' },
-  //   { id: 5, name: 'California', region: 'West' },
-  //   { id: 6, name: 'Colorado', region: 'West' },
-  //   { id: 7, name: 'Connecticut', region: 'Northeast' },
-  //   { id: 8, name: 'Delaware', region: 'South' },
-  //   { id: 9, name: 'Florida', region: 'South' },
-  //   { id: 10, name: 'Georgia', region: 'South' },
-  //   { id: 11, name: 'Hawaii', region: 'West' },
-  //   { id: 12, name: 'Idaho', region: 'West' },
-  //   { id: 13, name: 'Illinois', region: 'Midwest' },
-  //   { id: 14, name: 'Indiana', region: 'Midwest' },
-  //   { id: 15, name: 'Iowa', region: 'Midwest' },
-  //   { id: 16, name: 'Kansas', region: 'Midwest' },
-  //   { id: 17, name: 'Kentucky', region: 'South' },
-  //   { id: 18, name: 'Louisiana', region: 'South' },
-  //   { id: 19, name: 'Maine', region: 'Northeast' },
-  //   { id: 21, name: 'Maryland', region: 'South' },
-  //   { id: 22, name: 'Massachusetts', region: 'Northeast' },
-  //   { id: 23, name: 'Michigan', region: 'Midwest' },
-  //   { id: 24, name: 'Minnesota', region: 'Midwest' },
-  //   { id: 25, name: 'Mississippi', region: 'South' },
-  //   { id: 26, name: 'Missouri', region: 'Midwest' },
-  //   { id: 27, name: 'Montana', region: 'West' },
-  //   { id: 28, name: 'Nebraska', region: 'Midwest' },
-  //   { id: 29, name: 'Nevada', region: 'West' },
-  //   { id: 30, name: 'New Hampshire', region: 'Northeast' },
-  //   { id: 31, name: 'New Jersey', region: 'Northeast' },
-  //   { id: 32, name: 'New Mexico', region: 'West' },
-  //   { id: 33, name: 'New York', region: 'Northeast' },
-  //   { id: 34, name: 'North Dakota', region: 'Midwest' },
-  //   { id: 35, name: 'North Carolina', region: 'South' },
-  //   { id: 36, name: 'Ohio', region: 'Midwest' },
-  //   { id: 37, name: 'Oklahoma', region: 'South' },
-  //   { id: 38, name: 'Oregon', region: 'West' },
-  //   { id: 39, name: 'Pennsylvania', region: 'Northeast' },
-  //   { id: 40, name: 'Rhode Island', region: 'Northeast' },
-  //   { id: 41, name: 'South Carolina', region: 'South' },
-  //   { id: 42, name: 'South Dakota', region: 'Midwest' },
-  //   { id: 43, name: 'Tennessee', region: 'South' },
-  //   { id: 44, name: 'Texas', region: 'South' },
-  //   { id: 45, name: 'Utah', region: 'West' },
-  //   { id: 46, name: 'Vermont', region: 'Northeast' },
-  //   { id: 47, name: 'Virginia', region: 'South' },
-  //   { id: 48, name: 'Washington', region: 'South' },
-  //   { id: 49, name: 'West Virginia', region: 'South' },
-  //   { id: 50, name: 'Wisconsin', region: 'Midwest' },
-  //   { id: 51, name: 'Wyoming', region: 'West' }
-  // ];
+  savingStatus: String;
 
   constructor(
     private router: Router,
@@ -132,16 +81,40 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.navigationSubscription = this.router.events.subscribe((e: any) => {
       // If it is a NavigationEnd event re-initalise the component
       if (e instanceof NavigationEnd) {
+        console.log("hiii");
+       var newstate = this.route.snapshot.params; 
+       
+        this.route.queryParams
+          .filter(params => params.data)
+          .subscribe(params => {
+            console.log("paramdata---")
+            console.log(params); // {order: "popular"}
+            console.log("paramdata-end--")
+    
+            this.updateData = params.data;
+            this.selectedJobStatus = params.status;
+            //  console.log(this.updateData); // popular
+          });
         //   this.initialiseInvites();
-        this.homeForm.reset({});
-        this.createdJobId = false;
-        this.updateData = false;
-        this.isApproved = false;
-        this.isDraft = false;
-        this.isShared = false;
-        this.isFinished = false;
+        console.log(this.actionState);
+        console.log(this.updateData);
+        if(this.actionState == "update" && this.updateData){
+          this.fetchJobDataById();
+        }
+        else{
+          this.homeForm.reset({});
+          this.createdJobId = false;
+          this.updateData = false;
+          this.isApproved = false;
+          this.isDraft = false;
+          this.isShared = false;
+          this.isFinished = false;
+        }
+    
+       
       }
     });
+
     this.homeForm = this.fb.group({
       'ad_title': ['', Validators.required],
       'ad_body': ['', Validators.required],
@@ -210,44 +183,12 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     });
 
-
-
-
-
-    this.state = this.route.snapshot.params.id;
-    this.route.queryParams
-      .filter(params => params.data)
-      .subscribe(params => {
-        //  console.log(params); // {order: "popular"}
-
-        this.updateData = params.data;
-        this.selectedJobStatus = params.status;
-        //   console.log(this.updateData); // popular
-      });
+    this.fetchUrlParams();
+   
 
     if (this.updateData) {
       this.actionState = "update";
-      this.loaderMsg = "Loading..";
-
-      this.spinner.show();
-      this.jobService.getJobListById(this.updateData)
-        .subscribe(
-          jobList => {
-            //response
-            console.log(jobList)
-            this.spinner.hide();
-            this.updatedData = jobList;
-            this.statusUpdate();
-            this.isOwner = jobList.is_owner;
-            //  console.log(this.isOwner);
-            this.homeForm.patchValue(jobList);
-
-          },
-          err => {
-            //  debugger;
-
-          }
-        );
+      this.fetchJobDataById();
 
     }
 
@@ -323,6 +264,48 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
     // console.log(this.serviceReply.biases.biases[0].key);
     this.getApprovalEmail();
+  }
+
+
+  fetchUrlParams(){
+    debugger;
+    this.state = this.route.snapshot.params.id;
+    this.route.queryParams
+      .filter(params => params.data)
+      .subscribe(params => {
+        console.log("paramdata---")
+        console.log(params); // {order: "popular"}
+        console.log("paramdata-end--")
+
+        this.updateData = params.data;
+        this.selectedJobStatus = params.status;
+        //  console.log(this.updateData); // popular
+      });
+
+  }
+
+  fetchJobDataById(){
+    this.loaderMsg = "Loading..";
+
+    this.spinner.show();
+    this.jobService.getJobListById(this.updateData)
+      .subscribe(
+        jobList => {
+          //response
+          console.log(jobList)
+          this.spinner.hide();
+          this.updatedData = jobList;
+          this.statusUpdate();
+          this.isOwner = jobList.is_owner;
+          //  console.log(this.isOwner);
+          this.homeForm.patchValue(jobList);
+
+        },
+        err => {
+          //  debugger;
+
+        }
+      );
   }
 
   statusUpdate() {
@@ -468,10 +451,8 @@ export class HomeComponent implements OnInit, OnDestroy {
           if (data.status) {
             this.closeFirstModal();
             this.templateMsg = "A notification has been sent to your Colleague’s email";
-
             this.modalRef = this.modalService.show(template);
             this.router.navigateByUrl('/joblisting');
-
           }
           else {
             this.alerts.push({
@@ -545,45 +526,45 @@ export class HomeComponent implements OnInit, OnDestroy {
 
       // this.updateJob(dataForm, this.updateData)
       //   .then((dataUpdate) => {
-          // start of approve job
-          this.jobService.approve({ jobad_id: this.updateData })
-            .subscribe(
-              jobList => {
-                //response
-                //  this.spinner.hide();
-                console.log(jobList);
-                this.spinner.hide();
-                if (jobList.is_approved == true) {
-                  this.router.navigateByUrl('/joblisting');
-                }
-                else {
-                  this.spinner.hide();
-                  this.templateMsg = "Unable To Approve Job :approve";
-                  this.modalRef = this.modalService.show(template);
+      // start of approve job
+      this.jobService.approve({ jobad_id: this.updateData })
+        .subscribe(
+          jobList => {
+            //response
+            //  this.spinner.hide();
+            console.log(jobList);
+            this.spinner.hide();
+            if (jobList.is_approved == true) {
+              this.router.navigateByUrl('/joblisting');
+            }
+            else {
+              this.spinner.hide();
+              this.templateMsg = "Unable To Approve Job :approve";
+              this.modalRef = this.modalService.show(template);
 
-                }
+            }
 
-                //  this.isOwner = !jobList.is_owner;
-                //  console.log(this.isOwner);
-                //   this.homeForm.patchValue(jobList);
-              },
-              err => {
-                //  debugger;
-                this.spinner.hide();
-                this.templateMsg = "Unable To Approve Job : approve";
+            //  this.isOwner = !jobList.is_owner;
+            //  console.log(this.isOwner);
+            //   this.homeForm.patchValue(jobList);
+          },
+          err => {
+            //  debugger;
+            this.spinner.hide();
+            this.templateMsg = "Unable To Approve Job : approve";
 
-                this.modalRef = this.modalService.show(template);
+            this.modalRef = this.modalService.show(template);
 
-              }
-            );
-          //end of approve job
-        // })
-        // .catch((err) => {
-        //   this.spinner.hide();
-        //   this.templateMsg = "Unable To Approve Job : update issue";
+          }
+        );
+      //end of approve job
+      // })
+      // .catch((err) => {
+      //   this.spinner.hide();
+      //   this.templateMsg = "Unable To Approve Job : update issue";
 
-        //   this.modalRef = this.modalService.show(template);
-        // })
+      //   this.modalRef = this.modalService.show(template);
+      // })
 
 
 
@@ -820,6 +801,59 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
 
+
+  createOnly(createData) {
+    return new Promise((resolve, reject) => {
+      this.jobService
+        .create(createData)
+        .subscribe(
+          updatedUser => {
+            resolve({ status: true, data: updatedUser });
+          },
+          err => {
+            reject({ status: false, data: err });
+          }
+        );
+    });
+
+  }
+
+  saveJob() {
+    this.savingStatus = "Saving..... ";
+    
+    var dataForm = {
+      "ad_body": this.homeForm.value.ad_body,
+      "ad_title": this.homeForm.value.ad_title,
+      "department": this.homeForm.value.department,
+      "city": this.homeForm.value.city,
+      "country": this.homeForm.value.country
+    };
+    if (this.updateData) {
+      this.updateJob(dataForm, this.updateData)
+        .then((datax: any) => {
+          this.savingStatus = "";
+          console.log(datax);
+          this.redirect(this.updateData, this.selectedJobStatus);
+        })
+        .catch((err) => {
+          this.savingStatus = "Error Occured while updating...";
+
+        });
+    }
+    else {
+      this.createOnly(dataForm)
+        .then((datax:any) => {
+          this.savingStatus = "";
+          this.redirect(datax.data.id, 0)
+        })
+        .catch((data) => {
+          this.savingStatus = "Error Occured while creating...";
+        });
+
+    }
+  }
+
+
   shareJob(id) {
     debugger;
     return new Promise(resolve => {
@@ -988,6 +1022,14 @@ export class HomeComponent implements OnInit, OnDestroy {
     if (this.navigationSubscription) {
       this.navigationSubscription.unsubscribe();
     }
+    this.actionState ="add"
+
+  }
+
+  redirect(id, status) {
+    debugger;
+    this.actionState ="update";
+    this.router.navigateByUrl('/dashboard/editor?data=' + id + '&status=' + status);
 
   }
 }
