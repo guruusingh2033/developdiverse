8ce0d202b803be5e8d0b03452ef4df040145207a
guruusingh2033 <guruu.singh2033@gmail.com>
2018-08-07 16:18:45 +0530

    bg fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -161,36 +161,36 @@ export class HomeComponent implements OnInit, OnDestroy {
       </ul>
       </div>`;
 
-
+      
     
       $(document).ready(function () {
         $(".dropdown").hide();
         $(".openDrp").click(function () {
           // alert("hi");
           $(".dropdown").toggle();
+          $(this).append(dropdown);
   
-          ////   $(this).append(dropdown);
   
+        //   var selectBox = document.querySelectorAll('.drop');
+        //   console.log(selectBox);
+        //   for(var i=0;i<selectBox.length;i++){
+        //     console.log(selectBox[i]);
+        //   // selectBox[i].addEventListener('click',function(){
   
-          // var selectBox = document.querySelectorAll('.drop');
-          // console.log(selectBox);
-          // for(var i=0;i<selectBox.length;i++){
-          //   console.log(selectBox[i]);
-          // selectBox[i].addEventListener('click',function(){
-  
-          // },false);
-          //  console.log("binded");
-          // }
-        });
+        //   // },false);
+        //    console.log("binded");
+        //   }
+          });
   
         $(".drop").click(function () {
           //alert("hello");
-          //  /     $(".dropdown").toggle();
+            console.log($(this).attr('id'));
+              $(".dropdown").toggle();
   
         });
   
       });
-
+      
     this.fetchUrlParams();
 
 
@@ -216,16 +216,12 @@ export class HomeComponent implements OnInit, OnDestroy {
 //     <div class="dropdown-ex">
 //   <button class="openDrp">Dropdown</button>
 //   <div class="dropdown" contenteditable="false">
-//   <ul class="dropdown-select">
-//     <li class="drop"><a>Data</a></li>
-//     <li class="drop" id=""><a>Data1</a></li>
-//     <li class="drop" id=""><a>Data2</a></li>
-//     <li class="drop" id=""><a>Data3</a></li>
-//   </ul>
-//   </div>
+//   </div>  
+
 // </div>
 //     `;
     this.ad_body = ``;
+
     //  this.ad_body =`Lorem <span id='ipsum' style='color:red' (click)='call()'>ipsum</span> is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over <span id='content' style='color:green' (click)='call()'>content</span>. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem <span id='ipsum' style='color:red' (click)='call()'>ipsum</span> is mostly a part of a Latin text by the classical author and philosopher Cicero. Its words and letters have been changed by addition or removal, so to deliberately render its <span id='content' style='color:green' (click)='call()'>content</span> nonsensical; it's not genuine, correct, or comprehensible Latin anymore. While lorem <span id='ipsum' style='color:red' (click)='call()'>ipsum</span>'s still resembles classical Latin, it actually has no meaning whatsoever. As Cicero's text doesn't contain the letters K, W, or Z, alien to latin, these, and others are often inserted randomly to mimic the typographic appearence of European languages, as are digraphs not to be found in the original.  In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual <span id='content' style='color:green' (click)='call()'>content</span> still not being ready. Think of a news blog that's filled with <span id='content' style='color:green' (click)='call()'>content</span> hourly on the day of going live. However, reviewers tend to be distracted by comprehensible <span id='content' style='color:green' (click)='call()'>content</span>, say, a random text copied from a newspaper or the internet. The are likely to focus on the text, disregarding the layout and its elements. Besides, random text risks to be unintendedly humorous or offensive, an unacceptable risk in corporate environments. Lorem <span id='ipsum' style='color:red' (click)='call()'>ipsum</span> and its many variants have been employed since the early 1960ies, and quite likely since the sixteenth century.` ; 
     var input = document.getElementById('myInput');
     this.serviceReply ={
@@ -555,11 +551,15 @@ export class HomeComponent implements OnInit, OnDestroy {
       }
       return !this.isShared && this.isDraft && this.isOwner && !this.isFinished;
     }
+    else if(button == "ownerfinish"){
+      return this.isApproved && this.isOwner && !this.isFinished;
+
+    }
     else if (button == "finishread") {
       if (!this.isOwner && this.isShared && this.isApproved && !this.isFinished) {
         this.isReadonly = true;
       }
-      return this.isOwner && this.isShared && this.isApproved && !this.isFinished;
+      return !this.isOwner && this.isShared && this.isApproved && !this.isFinished;
 
     }
     else if (button == "finishr") {
@@ -1139,6 +1139,6 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 
 function fire(data) {
-  //console.log(data);
+  console.log(data);
   $(".dropdown").hide();
 }
